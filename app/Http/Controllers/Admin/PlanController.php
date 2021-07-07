a80b380b00fe90a3d8f7822dcabb89ce1eab4501
Diego Danni <diego.danni@gmail.com>
2021-07-07 17:35:22 -0300

    admin lte

---
+++ app/Http/Controllers/Admin/PlanController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PlanController extends Controller
+{
+ return view('admin.pages.plans.index');
+}
